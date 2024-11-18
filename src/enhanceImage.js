a8302f6b18111c1f4cc1c53de7e621d534463975
shailesh-doctena <127737937+shailesh-doctena@users.noreply.github.com>
2024-11-18 16:14:43 +0530

    wip

--- src/enhanceImage.js
+++ src/enhanceImage.js
@@ -1,16 +1,16 @@
-import { v2 as cloudinary } from 'cloudinary';
+const { v2 } = require('cloudinary');
 
 (async function() {
 
     // Configuration
-    cloudinary.config({ 
+    v2.cloudinary.config({ 
         cloud_name: 'dcrkuqiqg', 
         api_key: '532828865417746', 
         api_secret: 'D9dyjtd1WErLKS8pqNTfNeR2OvY' // Click 'View API Keys' above to copy your API secret
     });
     
     // Upload an image
-     const uploadResult = await cloudinary.uploader
+     const uploadResult = await v2.cloudinary.uploader
        .upload(
            'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
                public_id: 'shoes',
@@ -23,7 +23,7 @@ import { v2 as cloudinary } from 'cloudinary';
     console.log(uploadResult);
     
     // Optimize delivery by resizing and applying auto-format and auto-quality
-    const optimizeUrl = cloudinary.url('shoes', {
+    const optimizeUrl = v2.cloudinary.url('shoes', {
         fetch_format: 'auto',
         quality: 'auto'
     });
@@ -31,7 +31,7 @@ import { v2 as cloudinary } from 'cloudinary';
     console.log(optimizeUrl);
     
     // Transform the image: auto-crop to square aspect_ratio
-    const autoCropUrl = cloudinary.url('shoes', {
+    const autoCropUrl = v2.cloudinary.url('shoes', {
         crop: 'auto',
         gravity: 'auto',
         width: 500,
