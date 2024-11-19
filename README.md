66abd064537b0a61620670688d4a1d9f42fdf0ac
shailesh-doctena <127737937+shailesh-doctena@users.noreply.github.com>
2024-11-19 10:25:51 +0530

    Added places parameter where we can provide what we find to nearest

--- README.md
+++ README.md
@@ -10,3 +10,34 @@ List of apps which are uses geolocation
 - Weather and News
 - Banking and Fintech
 - Emergency and Safety
+
+API can find blow places 
+- Food & Drink
+    - restaurant
+    - café
+    - bar
+    - bakery
+- Shops & Services
+    - store
+    - shopping_mall
+    - supermarket
+    - laundry
+- Health & Medical
+    - hospital
+    - pharmacy
+    - dentist
+- Entertainment & Recreation
+    - movie_theater
+    - museum
+    - park
+    - gym
+- Finance & Government
+    - atm
+    - bank
+    - post_office
+- Travel & Transportation
+    - airport
+    - bus_station
+    - train_station
+    - car_rental
+
--- geo-location-apis/src/modules/near-by-places/controller.js
+++ geo-location-apis/src/modules/near-by-places/controller.js
@@ -1,21 +1,20 @@
 // controllers/userController.js
 const GOOGLE_API_KEY = 'AIzaSyDbeuYo4lbljyiTl-aQNto-6BUQBttkaV4';
 const axios = require('axios');
+const GEOCODEURL = 'https://maps.googleapis.com/maps/api/geocode/json';
+const PLACEURL = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json';
 
 // Get all users
 exports.nearPlaces = async (req, res) => {
   try {
-    const address = req.body.address;
-    console.log('address', address);
+    const {address, place} = req.body;
     const coordinates = await getCoordinatesFromAddress(address);
     if (!coordinates) {
-        console.log('Could not get coordinates.');
-        return;
+      console.log('Could not get coordinates.');
+      return;
     }
-
-    const hospitals = await findNearbyHospitals(coordinates.lat, coordinates.lng);
-    console.log('Nearby Hospitals:', hospitals);
-    res.json({ hospitals });
+    const data = await findNearbyHospitals(coordinates.lat, coordinates.lng, place);
+    res.json({ data });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -23,32 +22,30 @@ exports.nearPlaces = async (req, res) => {
 
 // Step 1: Convert address to latitude and longitude
 async function getCoordinatesFromAddress(address) {
-const geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${GOOGLE_API_KEY}`;
-
-    try {
-        const response = await axios.get(geocodeUrl);
-        console.log('response', response);
-        const location = response.data.results[0].geometry.location;
-        return location; // { lat: ..., lng: ... }
-    } catch (error) {
-        console.error('Error getting coordinates:', error);
-        return null;
-    }
+const geocodeUrl = `${GEOCODEURL}?address=${encodeURIComponent(address)}&key=${GOOGLE_API_KEY}`;
+  try {
+    const response = await axios.get(geocodeUrl);
+    const location = response.data.results[0].geometry.location;
+    return location; // { lat: ..., lng: ... }
+  } catch (error) {
+    console.error('Error getting coordinates:', error);
+    return null;
+  }
 }
 
 // Step 2: Find nearby hospitals
-async function findNearbyHospitals(lat, lng) {
-const placesUrl = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=5000&type=hospital&key=${GOOGLE_API_KEY}`;
-    try {
-        const response = await axios.get(placesUrl);
-        return response.data.results.map(hospital => ({
-        name: hospital.name,
-        address: hospital.vicinity,
-        rating: hospital.rating,
-        userRatingsTotal: hospital.user_ratings_total
-        }));
-    } catch (error) {
-        console.error('Error finding hospitals:', error);
-        return [];
-    }
+async function findNearbyHospitals(lat, lng, place) {
+const placesUrl = `${PLACEURL}?location=${lat},${lng}&radius=5000&type=${place}&key=${GOOGLE_API_KEY}`;
+  try {
+    const response = await axios.get(placesUrl);
+    return response.data.results.map(item => ({
+      name: item.name,
+      address: item.vicinity,
+      rating: item.rating,
+      userRatingsTotal: item.user_ratings_total
+    }));
+  } catch (error) {
+    console.error('Error finding hospitals:', error);
+    return [];
+  }
 }
